b5d9046aa4e8c256181bc5d35b0700be8ec0f929
Dipeen Dahal <dipeendahal65@gmail.com>
2026-04-10 12:57:13 +0545

    GCD update

---
+++ Recursion/GCD.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class GCD {
+    public static void main(String args[]){
+        System.out.println(""+gcd(12, 20));
+    }
+
+    public static int gcd(int a,int b){
+        if(b==0){
+            return a;
+        }
+        else{
+            return gcd(b, a%b);
+        }
+    }
+}
